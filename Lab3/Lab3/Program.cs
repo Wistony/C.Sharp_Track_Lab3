0abdeba996c7b923b8ccd2f335ccaa0e3f237b89
Max.Androshchuk <mmaksimm921@gmail.com>
2020-03-23 18:10:38 +0200

    Add game cycle with switch block in main method

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -9,22 +9,61 @@ namespace Lab3
             var game = new Sudoku(3);
             var gameHistory = new GameHistory();
             
-            while (game.SudokuPuzzle.NotFull())
+            game.CreatePuzzle();
+            var flag = true;
+            while (flag)
             {
                 ConsoleOutput.Print(game.SudokuPuzzle.ConvertToString().ToString());
-                
-                var i = Validation.InputValue("Enter row number: ") - 1;
-                var j = Validation.InputValue("Enter column number: ") - 1;
-                var value = Validation.InputValue("Enter cell value: ");
 
-                if(game.SudokuPuzzle.CellIsEmpty(i, j))
-                { 
-                    game.SudokuPuzzle.AddCellValue(i, j, value);
-                    gameHistory.History.Push(game.SaveState(i,j,value));
-                }
-                else
+                Console.WriteLine(" 1)Continue game");
+                Console.WriteLine(" 2)Undo");
+                Console.WriteLine(" 3)Exit");
+                var str = Console.ReadLine();
+                var choice = int.Parse(str);
+                switch (choice)
                 {
-                    Console.WriteLine("This cell already has a value");
+                    case 1:
+                    {
+                        var i = Validation.InputValue("Enter row number: ") - 1;
+                        var j = Validation.InputValue("Enter column number: ") - 1;
+                        var value = Validation.InputValue("Enter cell value: ");
+
+                        if(game.SudokuPuzzle.CellIsEmpty(i, j))
+                        { 
+                            game.SudokuPuzzle.AddCellValue(i, j, value);
+                            gameHistory.History.Push(game.SaveState(i,j,value));
+                        }
+                        else
+                        {
+                            Console.WriteLine("This cell already has a value");
+                        }
+                        flag = game.SudokuPuzzle.NotFull();
+                        break;
+                    }
+                    case 2:
+                    {
+                        if (!gameHistory.IsEmpty())
+                        {
+                            game.RestoreState(gameHistory.Undo());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Game history is empty");
+                        }
+                        flag = game.SudokuPuzzle.NotFull();
+                        break;
+                    }
+                    case 3:
+                    {
+                        flag = false;
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Enter number from 1 to 3");
+                        flag = game.SudokuPuzzle.NotFull();
+                        break;
+                    }
                 }
             }
             
